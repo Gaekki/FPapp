1c11c16dc55b362244ab7384a2b283bdb58c0e93
dw <imdongwook@gmail.com>
2020-02-10 16:10:44 +0900

    1st time

---
+++ app/src/main/java/com/example/fpapp/Name_list_activity.java
@@ -0,0 +1,35 @@
+package com.example.fpapp;
+
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class Name_list_activity extends MainActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.name_list);
+
+        int currentScreenOrientation = this.getResources().getConfiguration().orientation;
+        recyclerView = findViewById(R.id.names_list);
+        recyclerView.setHasFixedSize(true);
+
+        // portrait mode vertical list
+        if (currentScreenOrientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+        // landscape mode horizontal list
+        if (currentScreenOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        }
+
+    }
+
+}
