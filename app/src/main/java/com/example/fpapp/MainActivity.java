747db15444462b1e2086717772c9f0fc6a9a4e38
dw <imdongwook@gmail.com>
2020-02-10 16:15:07 +0900

    2nd time

---
+++ app/src/main/java/com/example/fpapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.fpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends login_page {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMain = findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        Button btnList = findViewById(R.id.btnList);
+        btnList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), Name_list_activity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button btnAI = findViewById(R.id.btnAI);
+        btnAI.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.fp_alert);
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/fpapp/login_page.java
@@ -0,0 +1,30 @@
+package com.example.fpapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+    }
+}
